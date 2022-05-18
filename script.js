e1d881b43051a3dcf722e3af2b81b30b538ced35
Alexander Lall <alexander.heddier@gmx.de>
2022-05-19 00:09:40 +0530

    Added responsive grid

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let isAllImagesAdded = false;
+let isAddingCompleted = false;
+let isLastLargeImageLeftAligned = false;
 let imagesAddedCount = 0;
 let imagesToAdd = [];
 
+const imagesPerGrid = 5
+const initialRowCount = 2;
+const standardRowCount = 6;
 const apiKey = 'CHHw0kLqvnOUB7u-iK6mzQO_4Nfj7m57oxHO5t-fUco';
-let imagesPerRequest = 5;
-const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imagesPerRequest}`;
+const initialApiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imagesPerGrid * initialRowCount}`;
+const standardApiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${imagesPerGrid * standardRowCount}`;
 
 function imageLoaded() {
     imagesAddedCount++;
     if(imagesAddedCount == imagesToAdd.length) {
-        imagesPerRequest = 30;
-        isAllImagesAdded = true;
+        isLoadingCompleted = true;
         loader.hidden = true;
     }
 }
 
-function getPhotosFromUnsplashApi() {
+function getPhotosFromUnsplashApi(apiUrl) {
     try {
         fetch(apiUrl)
         .then(response => response.json())
@@ -32,7 +35,30 @@ function getPhotosFromUnsplashApi() {
 }
 
 function addImagesToDom(){
-    imagesToAdd.forEach(imgData => {
+    for(let i = 0; i < imagesToAdd.length / imagesPerGrid; i++) {
+        const index = i * imagesPerGrid; 
+
+        // Image Grid
+        let imageGrid = document.createElement('div');
+        imageGrid.classList.add('image-grid');
+
+        // Large image
+        let largeImage = createImage(imagesToAdd[index]);
+        largeImage.classList.add(isLastLargeImageLeftAligned ? 'large-right-aligned' : 'large-left-aligned')
+        isLastLargeImageLeftAligned = !isLastLargeImageLeftAligned;
+        imageGrid.appendChild(largeImage);
+
+        // Other smaller images
+        for(let j = 1; j < imagesPerGrid; j++) {
+            imageGrid.appendChild(createImage(imagesToAdd[index + j]));
+        }
+
+        imageContainer.appendChild(imageGrid);
+    }
+}
+
+// Helper Functions
+function createImage(imgData) {
     // Create <a> to link to Unsplash
     let a = document.createElement('a');
     setAttributes(a, {
@@ -42,19 +68,17 @@ function addImagesToDom(){
     // Create <img> for photo
     let img = document.createElement('img');
     setAttributes(img, {
-        src: imgData.urls.thumb,
+        src: imgData.urls.small,
         alt: imgData.alt_description,
         title: imgData.alt_description
     });
     // Update state after picture is loaded
     img.addEventListener('load', imageLoaded);
-    // Put <img> inside <a>, then <a> into the image container
+    // Put <img> inside <a>, and return <a>
     a.appendChild(img);
-    imageContainer.appendChild(a);
-    });
+    return a;
 }
 
-// Helper Functions
 function setAttributes(element, attributes) {
     for (const key in attributes) {
         element.setAttribute(key, attributes[key]);
@@ -63,12 +87,12 @@ function setAttributes(element, attributes) {
 
 // Event Listeners
 window.addEventListener('scroll', () => {
-    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && isAllImagesAdded) {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && isLoadingCompleted) {
         imagesAddedCount = 0;
-        isAllImagesAdded = false;
-        getPhotosFromUnsplashApi();
+        isLoadingCompleted = false;
+        getPhotosFromUnsplashApi(standardApiUrl);
     }
 });
 
 // On Load
-getPhotosFromUnsplashApi();
+getPhotosFromUnsplashApi(initialApiUrl);
